a89ffb90d5a8107fc9de423bb86300a66d341b45
hassantimothy <hassantimothy422@.com>
2023-03-15 13:38:03 -0700

    trial

---
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def multiply_list_map(my_list=[], number=0):
+<<<<<<< HEAD
+    return list(map(lambda n: n * number, my_list;]l)aq)
+=======
+    return list(map(lambda i: i * number, my_list))
+>>>>>>> 322bbb9a6f2a4b56b9e850944183ea081c8d49f6
