102e5b44dae233ae06bb32bfb3caa80fe790bd0e
hassantimothy <hassantimothy422@.com>
2023-04-12 11:18:22 -0700

    atats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 """Reads from standard input and computes metrics.
+Print accumulated metrics
 After every ten lines or the input of a keyboard interruption (CTRL + C),
 prints the following statistics:
     - Total file size up to that point.
